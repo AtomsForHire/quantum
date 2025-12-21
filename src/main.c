2eb18c19179008b1065ab78bc3d8a7c6858d52d9
Atom <tbjong2000@hotmail.com>
2025-12-21 21:46:40 +1100

    Move basis.c to basis dir

--- src/main.c
+++ src/main.c
@@ -1,12 +1,12 @@
+#include "grid/grid.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "grid.h"
 
 int main() {
   printf("Hello, World!\n");
 
-  //Haadi adding first: see if the radial grid is working:
-  RGrid* RGrid = create_rgrid(1,1,10.0);
+  // Haadi adding first: see if the radial grid is working:
+  RGrid *RGrid = create_rgrid(1, 1, 10.0);
   destroy_rgrid(RGrid);
 
   return EXIT_SUCCESS;
